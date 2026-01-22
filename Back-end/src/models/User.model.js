d9112457891952d5cfce259bb7958fa488709cbf
SAY007-DEV <mail.to.sayooj.kp@gmail.com>
2026-01-22 11:00:40 +0530

    user model added

---
+++ Back-end/src/models/User.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, unique: true },
+    password: String,
+    shopName: String
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
